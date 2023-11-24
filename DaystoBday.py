f04728fdd7dbcf57f24649d97c07c6dd271a14cf
Nathan Williams <440137@edu.cwa.ac.uk>
2023-11-24 12:07:02 +0000

    work

---
+++ DaystoBday.py
@@ -0,0 +1,20 @@
+# Exercise - Number of days till your birthday
+
+
+# Exercise 2: How many days until your birthday
+
+import datetime as dt
+
+userinput = print("DD/MM Born")
+
+day, month = userinput.split("/")
+
+print(f"Day: {day}, Month: {month}")
+
+
+
+din = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+total = 0
+din2 = [total := total + 1 for i in din]
+
+print (din2)
--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("my first python program")
+import datetime as dt
+
+userinput = input("Enter your date of birth (DD/MM/YYYY)")
+
+day , month, year = userinput.split("/")
+print (f"Day: {day}, Month: {month}, Year: {year}")
+
+dayborn = int(day)
+monthborn = int(month)
+yearborn= int(year)
+
+yearnow = dt.datetime.now().year
+monthnow = dt.datetime.now().month
+daynow = dt.datetime.now().day
+
+hashadbirthday = monthnow > monthborn or (monthnow == monthborn and daynow >= dayborn)
+
+if hashadbirthday:
+    age = yearnow - yearborn
+else:
+    age = yearnow - yearborn - 1
+
+print(f"You are {age} years old.")
